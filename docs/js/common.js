4444bd40ebca3c2c9175f6a162d06944a3143bd1
huynhsamha <huynhsamha@gmail.com>
2018-03-12 23:31:55 +0700

    add support, prism, remove github gist, fix ui

--- docs/js/common.js
+++ docs/js/common.js
@@ -1,7 +1,10 @@
 (() => {
 
+  // ngclipboard is modulecopy to clipboard
+  // duScroll is module to scroll anchor smooth and more
   const app = angular.module('jsConvertApp', ['ngclipboard', 'duScroll']);
 
+  // directive for select all text when click to textarea or input
   app.directive('selectOnClick', ['$window', function ($window) {
     return {
       restrict: 'A',
@@ -16,9 +19,30 @@
     };
   }]);
 
+
+  // directive for prism highlight code, use for binding with angular
+  app.directive('ngPrism', [function () {
+    return {
+      restrict: 'A',
+      scope: {
+        source: '@'
+      },
+      link(scope, element, attrs) {
+        scope.$watch('source', (v) => {
+          if (v) {
+            window.Prism.highlightElement(element.find('code')[0]);
+          }
+        });
+      },
+      template: '<code ng-bind=\'source\'></code>'
+    };
+  }]);
+
+
+  // Common Controller for all page html in this project
   app.controller('CommonController', ['$scope', '$document', ($scope, $document) => {
 
-    // Scroll Top
+    // Scroll Top Button
     $scope.showScrollTop = false;
     $document.on('scroll', () => {
       $scope.showScrollTop = $document.scrollTop() > 320;
@@ -27,7 +51,13 @@
 
   }]);
 
-  window.sr = ScrollReveal();
-  sr.reveal('.scrollreveal', { duration: 1100 });
+
+  // Scroll Reveal is package for reveal element on scroll to it with animation.
+  if (window.ScrollReveal) {
+    // check if have package scroll reveal, config reveal
+    // reveal all element with class scrollreveal
+    window.sr = ScrollReveal();
+    sr.reveal('.scrollreveal', { duration: 1100 });
+  }
 
 })();
