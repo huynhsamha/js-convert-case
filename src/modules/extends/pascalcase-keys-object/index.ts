65c8dee329d018fb5f73d9a0378e37a2e68bf57c
Ha Huynh <huynhsamha@gmail.com>
2020-08-18 03:39:01 +0700

    add feature recursive in subkey with value is array

--- src/modules/extends/pascalcase-keys-object/index.ts
+++ src/modules/extends/pascalcase-keys-object/index.ts
@@ -1,12 +1,14 @@
-import { isValidObject, Options, DefaultOption } from '../utils';
+import { isValidObject, Options, DefaultOption, isArrayObject, validateOptions } from '../utils';
 import toPascalCase from '../../js-pascalcase';
 
 /**
  * Convert string keys in an object to PascalCase format.
- * If `obj` isn't a json object, `null` is returned.
+ * @param obj: object to convert keys. If `obj` isn't a json object, `null` is returned.
+ * @param opt: (optional) Options parameter, default is non-recursive.
  */
 export default function pascalKeys(obj: any, opt: Options = DefaultOption): object | null {
   if (!isValidObject(obj)) return null;
+  opt = validateOptions(opt);
 
   const res: any = {};
   Object.keys(obj).forEach((key) => {
@@ -15,6 +17,20 @@ export default function pascalKeys(obj: any, opt: Options = DefaultOption): obje
     if (opt.recursive) {
       if (isValidObject(value)) {
         value = pascalKeys(value, opt);
+      } else if (opt.recursiveInArray && isArrayObject(value)) {
+        value = [...value].map((v) => {
+          let ret = v;
+          if (isValidObject(v)) {
+            // object in array
+            ret = pascalKeys(v, opt);
+          } else if (isArrayObject(v)) {
+            // array in array
+            // workaround by using an object holding array value
+            const temp: any = pascalKeys({ key: v }, opt);
+            ret = temp.key;
+          }
+          return ret;
+        });
       }
     }
     res[nkey] = value;
