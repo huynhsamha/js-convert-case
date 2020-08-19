1b7b3d0d1c9a1d66eec443b0241630e32771853b
Ha Huynh <huynhsamha@gmail.com>
2020-08-19 21:08:43 +0700

    update script building release files

--- scripts/build_release_binary.sh
+++ scripts/build_release_binary.sh
@@ -9,16 +9,45 @@ RELEASES="$ROOT/releases"
 DIST="$ROOT/dist"
 LIB="$ROOT/lib"
 
+rm -rf "$RELEASES"
+
+echo
+echo "Build packge for both dist and lib..."
+echo
+
 DIR="$PACKAGE-$VERSION"
 TAR_GZ="$DIR.tar.gz"
 TAR="$DIR.tar"
 ZIP="$DIR.zip"
 
-rm -rf "$RELEASES"
-
 mkdir -p "$RELEASES/$DIR"
 
 cp -R "$DIST" "$LIB" "$RELEASES/$DIR"
+
+cd "$RELEASES"
+
+echo "Make $TAR_GZ"
+tar -czf "$TAR_GZ" "$DIR"
+echo "Make $TAR"
+tar -cf "$TAR" "$DIR"
+echo "Make $ZIP"
+zip -rq "$ZIP" "$DIR"
+
+cd "../"
+echo
+echo "======================================"
+echo
+echo "Build packge for browser..."
+echo
+
+DIR="$PACKAGE-browser-$VERSION"
+TAR_GZ="$DIR.tar.gz"
+TAR="$DIR.tar"
+ZIP="$DIR.zip"
+
+mkdir -p "$RELEASES/$DIR"
+
+cp -R "$DIST" "$RELEASES/$DIR"
 
 cd "$RELEASES"
 echo "Make $TAR_GZ"
@@ -28,7 +57,9 @@ tar -cf "$TAR" "$DIR"
 echo "Make $ZIP"
 zip -rq "$ZIP" "$DIR"
 
-cd "$ROOT"
+cd "../"
+echo
+echo "======================================"
 echo
 echo "Release files:"
 echo
